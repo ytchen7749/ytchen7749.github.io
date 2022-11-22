131640f1cd8ad52be0eb640f4fcfee6df17c3b58
ytchen7749 <112504234+ytchen7749@users.noreply.github.com>
2022-11-22 18:47:11 +0800

    update

--- Webtech/AS7/js/control.js
+++ Webtech/AS7/js/control.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
     var max_scroll = $(document).height() - $(window).height() - 2400;
     $(window).scroll(function (e) {
-        //document.querySelector('#video').play();
+        //
         var video = $('#video').get(0),
-            videoLength = video.duration,
+        //    videoLength = video.duration,
             current_pos = $(document).scrollTop();
-        video.currentTime = videoLength * (current_pos / max_scroll);
+        //video.currentTime = videoLength * (current_pos / max_scroll);
+        if (current_pos > 14000) {
+            document.querySelector('#video').play();
+        }
     });
 })
