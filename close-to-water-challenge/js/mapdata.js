d0da28a998db73712ef4ee0ebd6ccd5e98bf56c0
ytchen7749 <112504234+ytchen7749@users.noreply.github.com>
2023-06-26 14:14:47 +0800

    clean css and modified content
    add loading, modified video control, add animation, modified CSS

---
+++ close-to-water-challenge/js/mapdata.js
@@ -0,0 +1,228 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#8d8d8d",
+    state_description: "",
+    state_color: "#ffffff",
+    state_hover_color: "off",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    location_description: "",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: "40",
+    location_type: "marker",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    location_pulse: "yes",
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+  
+    //Zoom settings
+    zoom: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    TWN1156: {
+      name: "高雄縣"
+    },
+    TWN1158: {
+      name: "屏東縣"
+    },
+    TWN1160: {
+      name: "台南縣"
+    },
+    TWN1161: {
+      name: "新竹市"
+    },
+    TWN1162: {
+      name: "新竹縣"
+    },
+    TWN1163: {
+      name: "宜蘭縣"
+    },
+    TWN1164: {
+      name: "基隆市"
+    },
+    TWN1165: {
+      name: "苗栗縣"
+    },
+    TWN1166: {
+      name: "台北市"
+    },
+    TWN1167: {
+      name: "新北市"
+    },
+    TWN1168: {
+      name: "桃園市"
+    },
+    TWN1169: {
+      name: "彰化縣"
+    },
+    TWN1170: {
+      name: "嘉義縣"
+    },
+    TWN1171: {
+      name: "嘉義市"
+    },
+    TWN1172: {
+      name: "花蓮縣"
+    },
+    TWN1173: {
+      name: "南投縣"
+    },
+    TWN1174: {
+      name: "台中市"
+    },
+    TWN1176: {
+      name: "雲林縣"
+    },
+    TWN1177: {
+      name: "台東縣"
+    },
+    TWN3414: {
+      name: "澎湖縣"
+    },
+    TWN3415: {
+      name: "金門縣"
+    },
+    TWN5128: {
+      name: "連江縣"
+    }
+  },
+  locations: {
+    "1": {
+      lat: "25.08742828",
+      lng: "121.6053422",
+      name: "大湖公園",
+      type: "marker",
+      color: "#FF0067",
+      description: "<br><div>禁止划船及游泳等水上活動。<br>\n主要禁止理由：大湖公園屬防災型的滯洪人工湖泊，划船等水域活動可能干擾棲息生物。</div><br><div>法源依據：台北市公園管理自治條例</div>"
+    },
+    "2": {
+      lat: "24.95808862",
+      lng: "121.5364331",
+      name: "碧潭",
+      type: "marker",
+      color: "#FF0067",
+      description: "<br><div>水上活動營運業者可在湖中開船。其他活動以游泳、獨木舟、無動力腳踏車和無動力橡皮艇為限。在2022年曾發生動力船和遊客在湖中相撞的意外。<div/><br><div>\n法源依據：\n新北市碧潭風景特定區水域遊憩活動管理辦法\n新北府觀管字第11006602441號\n</div>"
+    },
+    "3": {
+      lat: "24.86250065",
+      lng: "121.2095326",
+      name: "龍潭大池",
+      color: "#FF0067",
+      type: "marker",
+      description: "<br><div>獲許可的營業業者可經營天鵝船與SUP等活動。一般民眾無法任意下水。</div><br><div>法源依據：桃園市公園管理自治條例</div>"
+    },
+    "4": {
+      lat: "24.81610775",
+      lng: "121.2891587",
+      name: "石門水庫",
+      color: "#FF0067",
+      type: "marker",
+      description: "<br><div>可經營遊艇事業。目前因《水利法》禁止水庫任意進行遊憩活動之故，相關單位研擬法條中，之後期望民眾可參與「阿姆坪水上運動訓練基地」的活動體驗水上活動，或進行國手水上運動訓練，但一般人無法任意下水。</div><br><div>法源依據：水利法</div>"
+    },
+    "5": {
+      lat: "24.77507608",
+      lng: "120.9709238",
+      name: "青草湖",
+      color: "#FF0067",
+      description: "<br><div>可參與「青草湖水域遊憩中心」所辦理的水上活動。但2023年3月後，因《水域遊憩管理辦法》修正，舊保險金跟不上新法規，因此暫停活動。</div><br><div>法源依據：新竹市公園管理自治條例</div>"
+    },
+    "6": {
+      lat: "24.14337859",
+      lng: "120.6841659",
+      name: "台中公園日月湖",
+      color: "#FF0067",
+      description: "<br><div>除承包廠商外，其餘民眾不可下水。</div><br><div>法源依據：臺中市公園及行道樹管理自治條例第十三條</div>"
+    },
+    "7": {
+      lat: "23.00167378",
+      lng: "120.1543724",
+      name: "台南運河",
+      color: "#FF0067",
+      description: "<br><div>特定活動會開放無動力筏進入，其他時間需在下水10天前申請。開放程度討論中。</div><br><div>法源依據：臺南市臺南運河限制水域遊憩活動及相關事項公告</div>"
+    },
+    "8": {
+      lat: "22.76480576",
+      lng: "121.1650338",
+      name: "台東森林公園活水湖",
+      color: "#FF0067",
+      description: "<br><div>除活動外，其餘時間若想下水，需事先申請，並保險和配有救生員。台東縣政府希望未來委外管理。</div><br><div>法源依據：臺東縣公園管理自治條例</div>"
+    },
+    "9": {
+      lat: "24.79581933",
+      lng: "121.7404605",
+      name: "龍潭湖",
+      color: "#FF0067",
+      description: "<br><div>除有經營許可的商家、縣府的大型活動與選手練習活動外，其餘水上活動禁止。</div><br><div>法源依據：府旅管字第1120058205B</div>"
+    },
+    "10": {
+      lat: "24.6613172",
+      lng: "121.8018557",
+      name: "冬山河",
+      color: "#FF0067",
+      description: "<br><div>部分區域開放營業許可商家經營水上活動，其餘區域未經許可則禁止。</div><br><div>法源依據：府旅管字第1120058205B</div>"
+    }
+  },
+  labels: {
+    "10": {
+      name: "B",
+      x: 708.7182,
+      y: 471.132,
+      parent_type: "location",
+      parent_id: "7"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
